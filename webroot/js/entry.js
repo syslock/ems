bb3962dd5fd24fe25f4d3e136de1c018f5d3f64b
syslock <syslock@gmx.de>
2012-10-27 23:27:53 +0200

    separater Beitrags-Speicherknopf

--- webroot/js/entry.js
+++ webroot/js/entry.js
@@ -187,8 +187,8 @@ function edit_entry( button )
 		content.contentEditable = true
 		if( title.innerHTML.length>0 ) content.focus()
 	}
-	button.innerHTML = LS("20120513161628","Speichern")
-	button.onclick = function(){ save_entry_plain(button) }
+	button.style.display="none"
+	$(".entry-save",button.parentNode)[0].style.display=""
 }
 
 function get_plain_text( element )
@@ -261,116 +261,7 @@ function save_entry_plain( button )
 			result = parse_result( result )
 		}})
 	}
-	button.innerHTML = LS("20120513162048","Bearbeiten")
-	button.onclick = function(){ edit_entry(button) }
-}
-
-function save_entry( button )
-{
-	var entry_id = button.parentNode.data.object_id
-	var title = $(".entry-title",button.parentNode)[0]
-	if( !title ) return;
-	var content = $(".entry-content",button.parentNode)[0]
-	if( !content ) return;
-	title.contentEditable = false
-	content.contentEditable = false
-	// 1.) Strukturnormalisierung:
-	var remove_list = []
-	// 1.a) Titel:
-	for( var i=0; i<title.childNodes.length; i++ )
-	{
-		var child = title.childNodes[i]
-		if( child.nodeName!="#text" )
-		{
-			remove_list.push( child )
-		}
-	}
-	// 1.b) Inhalt:
-	for( var i=0; i<content.childNodes.length; i++ )
-	{
-		var child = content.childNodes[i]
-		if( child.nodeName=="#text" )
-		{
-			var data = child.nodeValue
-			new_div = new_text_part(entry_id, data)
-			if( new_div==undefined ) return;
-			$(child).before( new_div )
-			$(child).remove()
-		}
-		else if( child.className=="entry-text" )
-		{
-			var extract_siblings = [];
-			for( var k=0; k<child.childNodes.length; k++ )
-			{
-				var sub = child.childNodes[k]
-				if( sub.nodeName!="#text" || extract_siblings.length>0 )
-				{
-					extract_siblings.push(sub);
-				}
-			}
-			extract_siblings.reverse()
-			for( k in extract_siblings )
-			{
-				var sub = extract_siblings[k]
-				$(sub).detach()
-				$(child).after( sub )
-			}
-		}
-		else
-		{
-			remove_list.push( child )
-		}
-	}
-	for( var i in remove_list )
-	{
-		$(remove_list[i]).remove()
-	}
-	// 2.) Speicherung und Sortierung:
-	// 2.a) Titel:
-	$.ajax({
-		url : "ems.wsgi?do=store&id="+String(entry_id),
-		type : "POST",
-		data : { title: title.innerHTML },
-		async : false,
-		success :
-	function( result )
-	{
-		result = parse_result( result )
-	}})
-	// 2.b) Inhalt:
-	var part_id_list = []
-	for( var i=0; i<content.childNodes.length; i++ )
-	{
-		var child = content.childNodes[i]
-		if( child.className=="entry-text" )
-		{
-			var part_id = Number( child.id.split("-").reverse()[0] )
-			part_id_list.push( part_id )
-			// FIXME: data muss vorkodiert werden!
-			$.ajax({
-				url : "ems.wsgi?do=store&id="+String(part_id)
-					+"&sequence="+String(10+i*10),
-				type : "POST",
-				data : { data: child.innerHTML },
-				async : false,
-				success :
-			function( result )
-			{
-				result = parse_result( result )
-			}})
-		}
-	}
-	// 3.) serverseitige Bereinigung der entfernten Elemente:
-	$.ajax({
-		url : "ems.wsgi?do=delete&parent_id="+String(entry_id)
-			+"&child_not_in="+part_id_list.join(","),
-		async : false,
-		success :
-	function( result )
-	{
-		result = parse_result( result )
-	}})
-	button.innerHTML = LS("20120513162048","Bearbeiten")
-	button.onclick = function(){ edit_entry(button) }
+	button.style.display="none"
+	$(".entry-edit",button.parentNode)[0].style.display=""
 }
 

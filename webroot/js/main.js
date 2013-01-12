8777b1240eefef5a8593b4e5c121998a65f466f1
syslock <syslock@gmx.de>
2013-01-12 22:04:40 +0100

    gesamten Initialisierungscode in ready-Handler verschoben

--- webroot/js/main.js
+++ webroot/js/main.js
@@ -2,24 +2,25 @@ require( ["jquery","util","login","entry","localization","user"],
 	function($) {
 		$(document).ready( function(){ 
 			try {
+				var lang = get_cookie( "lang" )
+				var menu_language = $(".menu-language")[0]
+				if( !lang && menu_language ) lang = menu_language.value;
+				if( lang )
+				{
+					change_language( lang );
+				}
+				var style = get_cookie( "style" )
+				var menu_style = $(".menu-style")[0]
+				if( !style && menu_style ) style = menu_style.value;
+				if( style )
+				{
+					change_style( style );
+				}
+				
 				init(); 
 			} catch( error ) {
 				show_error( String(error) );
 			}
 		});
-		var lang = get_cookie( "lang" )
-		var menu_language = $(".menu-language")[0]
-		if( !lang && menu_language ) lang = menu_language.value;
-		if( lang )
-		{
-			change_language( lang );
-		}
-		var style = get_cookie( "style" )
-		var menu_style = $(".menu-style")[0]
-		if( !style && menu_style ) style = menu_style.value;
-		if( style )
-		{
-			change_style( style );
-		}
 	}
 );

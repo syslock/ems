5406645636412851a1aecc2d8e772587ca9c9936
syslock <syslock@gmx.de>
2015-02-28 00:22:58 +0100

    more flexible lexing for different variations of character chunks

--- rebuild_search_index.py
+++ rebuild_search_index.py
@@ -10,6 +10,9 @@ scan_sources = [
 	("type", "objects", "id", "type", 1),
 ]
 
+# list from: http://fasforward.com/list-of-european-special-characters/
+eu_chars = "¡¿ÄäÀàÁáÂâÃãÅåǍǎĄąĂăÆæÇçĆćĈĉČčĎđĐďðÈèÉéÊêËëĚěĘęĜĝĢģĤĥÌìÍíÎîÏïĴĵĶķĹĺĻļŁłĽľÑñŃńŇňÖöÒòÓóÔôÕõŐőØøŒœŔŕŘřẞßŚśŜŝŞşŠšŤťŢţÞþÜüÙùÚúÛûŰűŨũŲųŮůŴŵÝýŸÿŶŷŹźŽžŻż"
+
 environ = { "REMOTE_ADDR" : "localhost" }
 
 app = application.Application( environ, lambda x,y: None, name="ems", path=os.path.dirname(sys.argv[0]) )
@@ -19,10 +22,18 @@ for scan_source_definition in scan_sources:
 	scan_time = int( time.time() )
 	rows = c.execute( """select %(id_column)s, %(data_column)s from %(scan_table)s""" % locals() ).fetchall()
 	for obj_id, data in rows:
-		words = [ word.lower() for word in re.split(r'[^a-zA-Z0-9äöüßÄÖÜ]',data) if word ]
+		words = []
+		# character chunks between whitespaces and several punctuation marks:
+		words += [(i,w) for i,w in enumerate([ word.lower() for word in re.split(r'[ \t\r\n,.;!?:%"=]', data) if word ])]
+		# chunks of latin and several derived characters, numbers and some currency symbols:
+		words += [(i,w) for i,w in enumerate([ word.lower() for word in re.split(r'[^a-zA-Z0-9$€£¢#%s]' % (eu_chars), data) if word ])]
+		# host and file names:
+		words += [(i,w) for i,w in enumerate([ word.lower() for word in re.split(r'[^a-zA-Z0-9\-_.$€£¢#%s]' % (eu_chars), data) if word ])]
+		# numbers only:
+		words += [(i,w) for i,w in enumerate([ word.lower() for word in re.split(r'[^0-9]', data) if word ])]
 		c.execute( """delete from keywords where object_id=? and scan_source=?""", [obj_id, scan_source] )
 		app.db.commit()
-		for pos, word in enumerate(words):
+		for pos, word in words:
 			c.execute( """insert into keywords (object_id, word, pos, rank, scan_source, scan_time)
 					values(?, ?, ?, ?, ?, ?)""", [obj_id, word, pos, rank, scan_source, scan_time] )
 			app.db.commit()

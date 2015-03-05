f19ff13164b59374e71cf0aa2271ed062d75d69e
syslock <syslock@gmx.de>
2015-03-05 19:03:52 +0100

    reengineered Minions class based on more flexible SearchBar and safer get_module()

--- webroot/js/minions.js
+++ webroot/js/minions.js
@@ -1,52 +1,67 @@
-var Minions = {
-	show_latest : function( dom_parent, limit ) {
-		if( !dom_parent ) {
-			show_error( "Minions-Container undefined ("+String(dom_parent)+")" );
-			return;
-		}
-		$(dom_parent).empty();
-		load_visible_objects( {type: 'application/x-obj.minion', dom_parent: dom_parent, limit: limit} );
-		add_file( {dom_parent:dom_parent, custom_class:"mini", 
-		custom_callback: function( parms ) {
-			var minion_obj = undefined;
-			if( parms.source_obj ) {
-				minion_obj = $(parms.source_obj.dom_object).closest(".ems-minion")[0];
-			}
-			if( !minion_obj ) {
-				// Neues Minion anlegen:
-				minion_obj = $('<div class="ems-minion"></div>')[0];
-				$(parms.obj.dom_object).wrap( minion_obj );
-				new_item( {obj:{type: "application/x-obj.minion"}, dom_object: minion_obj} );
-				var minion_id = $(minion_obj).data().obj.id;
-				$.ajax({
-						url : "ems.wsgi?do=store&id="+parms.obj.id+"&parent_id="+String(minion_id),
-						type : "GET",
-						async : false, /* hier, ohne komplizierteres Event-Hanlding, wichtig zur Vermeidung von Race-Conditions */
-						success :
-					function( result ) {
-						result = parse_result( result )
-						if( result.succeeded ) {
-							Minions.show_latest( dom_parent, limit );
+var Minions = function( parms ) {
+	my = this;
+	my.dom_parent = parms.dom_parent;
+	my.limit = parms.limit;
+	
+	my.add_drop_box = function( dom_parent, limit ) {
+		add_file( {
+			dom_parent : my.dom_parent, 
+			custom_class : "mini", 
+			custom_callback : function( parms ) {
+				var minion_obj = undefined;
+				if( parms.source_obj ) {
+					minion_obj = $(parms.source_obj.dom_object).closest(".ems-minion")[0];
+				}
+				if( !minion_obj ) {
+					// Neues Minion anlegen:
+					minion_obj = $('<div class="ems-minion"></div>')[0];
+					$(parms.obj.dom_object).wrap( minion_obj );
+					new_item( {obj:{type: "application/x-obj.minion"}, dom_object: minion_obj} );
+					var minion_id = $(minion_obj).data().obj.id;
+					get_module( "store", {
+						args : {id : parms.obj.id, parent_id : minion_id},
+						async : false,
+						done : function( result ) {
+							result = parse_result( result )
+							if( result.succeeded ) {
+								my.search.search();
+							}
 						}
-					}
-				})
-			} else {
-				// FIXME: Das hat so keinen Zweck, solange wir Objekte in get.py grundsätzlich nach "ctime desc" selektieren!
-				// Altes Minion durch mtime-Update nach vorn ziehen:
-				var minion_id = $(minion_obj).data().obj.id;
-				$.ajax({
-						url : "ems.wsgi?do=store&id="+String(minion_id),
-						type : "GET",
-						async : false, /* hier, ohne komplizierteres Event-Hanlding, wichtig zur Vermeidung von Race-Conditions */
-						success :
-					function( result ) {
-						result = parse_result( result )
-						if( result.succeeded ) {
-							Minions.show_latest( dom_parent, limit );
+					});
+				} else {
+					// Altes Minion durch mtime-Update nach vorn ziehen:
+					var minion_id = $(minion_obj).data().obj.id;
+					get_module( "store", {
+						args : {id : minion_id},
+						async : false,
+						done : function( result ) {
+							result = parse_result( result )
+							if( result.succeeded ) {
+								my.search.search();
+							}
 						}
-					}
-				})
+					});
+				}
 			}
-		}}); 
+		}); 
 	}
+	
+	my.search = new SearchBar( {
+		result_handler : function( result ) {
+			result.dom_parent = my.dom_parent;
+			show_search_result( result );
+		},
+		result_types : 'application/x-obj.minion',
+		empty_search : 'type:minion',
+		order_by : 'mtime',
+		order_reverse : 'true',
+		range_limit : my.limit,
+		new_search_handler : function( parms ) {
+			my.dom_parent.empty();
+			my.add_drop_box();
+		},
+		on_ready : function() {
+			this.search();
+		}
+	} );
 };

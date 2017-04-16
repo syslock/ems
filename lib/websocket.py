0e6b1231a59d5b8ad463fa0c44648779ce8123af
syslock <syslock@gmx.de>
2017-04-16 15:01:34 +0200

    cleaned up client frame parsing and communication loop

--- lib/websocket.py
+++ lib/websocket.py
@@ -17,10 +17,10 @@ class WSListener():
 				con, addr = s.accept()
 				try:
 					ws_server = WSServer( con, addr )
-					ws_server.start()
 				except Exception as e:
 					sys.stderr.write( "\n".join(traceback.format_exception(Exception, e, e.__traceback__)) )
 					continue
+				ws_server.start()
 		except:
 			s.close()
 			raise
@@ -79,12 +79,15 @@ class WSServer( threading.Thread ):
 	def run( self ):
 		def read_thread():
 			while not self.quitting:
-				data = self.ws.read(1000)
+				data = self.ws.communicate()
 				if data:
 					self.con.send( data )
 				else:
-					print( "%s disconnected." % (str(self.addr)) )
-					break
+					self.module.sleep( self )
+				if self.ws.quitting:
+					self.app.trace( "WebSocket-Shutdown (%s)" % (self.app.query.remote_addr) )
+					print( "WebSocket-Shutdown (%s)" % (self.app.query.remote_addr) )
+					self.stop()
 		t = threading.Thread( target=read_thread )
 		t.start()
 		self.app.open_db() # App-DB für diesen Thread neu öffnen...
@@ -92,6 +95,7 @@ class WSServer( threading.Thread ):
 		while not self.quitting:
 			self.module.run( self )
 			self.module.sleep( self )
+		t.join()
 
 class WebSocket:
 	def __init__( self, app, socket, endpoint=None ):
@@ -112,13 +116,13 @@ class WebSocket:
 		self.prepare_handshake()
 		#self.input = app.query.environ["wsgi.input"]
 		self.socket = socket
-		self.client_data = b""
+		self.client_messages = []
 		self.client_data_semaphore = threading.Semaphore()
 		self.server_data = b""
 		self.server_data_semaphore = threading.Semaphore()
 		self.server_data_sent_offset = 0
 		self.quitting = False
-		self.client_reader = threading.Thread( target=self.read_client_bytes )
+		self.client_reader = threading.Thread( target=self.read_frames_from_client )
 		self.client_reader.start()
 	
 	def prepare_handshake( self ):
@@ -165,76 +169,74 @@ class WebSocket:
 			raise errors.StateError( "Encountered unsupported payload length of %d" % (length) )
 		frame += payload
 		return frame
-	
-	def decode_frame( self, frame ):
-		if len(frame)<2:
-			return 0, ""
-		op, length = struct.unpack( ">BB", frame[0:2] )
-		fin = (op & 2**7)!=0
-		op &= 2**4-1
-		masked = (length & 2**7)!=0
-		length &= 2**7-1
-		if not fin:
-			raise NotImplementedError( "FIXME: Unable to handle fragmented frames" )
-		if op not in (1,8,9,10):
-			raise NotImplementedError( "FIXME: Only text messages supported" )
-		if op==8:
-			self.app.trace( "WebSocket CLOSE received from client %s" % (self.app.query.remote_addr) )
-			self.quitting = True
-		if op==9:
-			self.app.trace( "WebSocket PING received from client %s" % (self.app.query.remote_addr) )
-		if op==10:
-			self.app.trace( "WebSocket PONG received from client %s" % (self.app.query.remote_addr) )
-		payload_offset = 2
-		if length == 126:
-			if len(frame)<payload_offset+2:
-				return 0, ""
-			length = struct.unpack( ">H", frame[2:2+2] )
-			payload_offset += 2
-		elif length == 127:
-			if len(frame)<payload_offset+8:
-				return 0, ""
-			length = struct.unpack( ">Q", frame[2:2+8] )
-			payload_offset += 8
-		if masked:
-			if len(frame)<payload_offset+4:
-				return 0, ""
-			mask = frame[ payload_offset : payload_offset+4 ]
-			payload_offset += 4
-		if len(frame)<payload_offset+length:
-			return 0, ""
-		payload = frame[ payload_offset : payload_offset+length ]
-		if masked:
-			decoded_payload = b""
-			for i in range(len(payload)):
-				decoded_payload += struct.pack( ">B", payload[i] ^ mask[i%4] )
-			payload = decoded_payload
-		if op==8 and len(payload)>=2:
-			# The Close frame MAY contain a body [...] If there is a body, the first 
-			# two bytes of the body MUST be a 2-byte unsigned integer (in network 
-			# byte order) representing a status code with value /code/ defined in 
-			# Section 7.4.
-			reason = struct.unpack( ">H", payload[0:2] )[0]
-			payload = payload[2:]
-			if len(payload):
-				reason_string = payload
-			else:
-				reason_string = "?"
-			self.app.trace( "WebSocket CLOSE reason from client %s: %d (%s)" % (self.app.query.remote_addr, reason, reason_string) )
-		return payload_offset+length, payload.decode("utf-8")
+		
+	class Frame():
+		def __init__( self, s ):
+			frame = bytes()
+			frame += s.recv(2)
+			op, length = struct.unpack( ">BB", frame[0:2] )
+			fin = (op & 2**7)!=0
+			op &= 2**4-1
+			masked = (length & 2**7)!=0
+			length &= 2**7-1
+			if not fin:
+				raise NotImplementedError( "FIXME: Unable to handle fragmented frames" )
+			if op not in (1,8,9,10):
+				raise NotImplementedError( "FIXME: Only text messages supported" )
+			if op==9:
+				pass #FIXME
+				#self.app.trace( "WebSocket PING received from client %s" % (self.app.query.remote_addr) )
+			if op==10:
+				pass #FIXME
+				#self.app.trace( "WebSocket PONG received from client %s" % (self.app.query.remote_addr) )
+			payload_offset = 2
+			if length == 126:
+				frame += s.recv(2)
+				length = struct.unpack( ">H", frame[2:2+2] )
+				payload_offset += 2
+			elif length == 127:
+				frame += s.recv(8)
+				length = struct.unpack( ">Q", frame[2:2+8] )
+				payload_offset += 8
+			if masked:
+				frame += s.recv(4)
+				mask = frame[ payload_offset : payload_offset+4 ]
+				payload_offset += 4
+			frame += s.recv(length)
+			payload = frame[ payload_offset : payload_offset+length ]
+			if masked:
+				decoded_payload = b""
+				for i in range(len(payload)):
+					decoded_payload += struct.pack( ">B", payload[i] ^ mask[i%4] )
+				payload = decoded_payload
+			if op==8:
+				self.reason = 0
+				if len(payload)>=2:
+					# The Close frame MAY contain a body [...] If there is a body, the first 
+					# two bytes of the body MUST be a 2-byte unsigned integer (in network 
+					# byte order) representing a status code with value /code/ defined in 
+					# Section 7.4.
+					self.reason = struct.unpack( ">H", payload[0:2] )[0]
+				payload = payload[2:]
+			self.op = op
+			self.close = self.op==8
+			self.payload = payload.decode("utf-8")
 			
-	def read_client_bytes( self ):
+	def read_frames_from_client( self ):
 		while not self.quitting:
 			try:
-				byte = self.socket.recv(1)
+				frame = self.Frame( self.socket )
 			except OSError as e:
 				self.app.trace( "WebSocket client %s hung up? Initiating server-side shutdown." % (self.app.query.remote_addr) )
 				self.quitting = True
 				return
+			if frame.close:
+				self.app.trace( "WebSocket CLOSE reason from client %s: %d (%s)" % (self.app.query.remote_addr, frame.reason, frame.payload) )
+				self.quitting = True
 			self.client_data_semaphore.acquire()
-			self.client_data += byte
+			self.client_messages.append( frame.payload )
 			self.client_data_semaphore.release()
-		self.app.trace( "WebSocket-Shutdown (read_client_bytes %s)" % (self.app.query.remote_addr) )
+		self.app.trace( "WebSocket-Shutdown (read_frames_from_client %s)" % (self.app.query.remote_addr) )
 	
 	def send( self, message ):
 		"""Highlevel thread-safe interface for sending messages to the client 
@@ -244,52 +246,32 @@ class WebSocket:
 		self.server_data += self.make_frame( message )
 		self.server_data_semaphore.release()
 	
-	def read( self, size ):
-		"""Responsible for sending data provided by the server-side application endpoint 
-			to the client and currently also for handing messages originating from the
+	def communicate( self ):
+		"""Forward data provided by the server-side application endpoint 
+			to the WebSocket server and forward messages originating from the
 			client up to the onmessage handler of the server-side application endpoint."""
-		while not self.quitting:
-			# 1.) Check if we can decode a complete frame from the client and hand it up
-			# to the server-side endpoint for consumtion:
-			self.client_data_semaphore.acquire()
-			try:
-				client_read_length, client_message = self.decode_frame( self.client_data )
-				if client_read_length>0:
-					self.client_data = self.client_data[client_read_length:]
-			except Exception as e:
-				# decode_frame should leave incomplete frames untouched, by returning a zero chunk,
-				# but must raise exceptions on fatal parse errors. Ignoring erroneous input would 
-				# leave the socket in an unusable exception loop. Discarding arbitrary chunks of 
-				# input on the other hand would probably lead to subsequent errors, so the only 
-				# sane option in this situation is to close the socket in a controlled manner:
-				self.app.log( "\n".join(traceback.format_exception(Exception, e, e.__traceback__)) )
-				self.close()
-				raise
-			self.client_data_semaphore.release()
-			if client_read_length>0:
-				self.onmessage( client_message )
-			# 2.) Check if we have frame data from send calls of the server-side endpoint,
-			# that we can hand down to the webserver:
-			self.server_data_semaphore.acquire()
-			server_chunk = self.server_data[0:size]
-			if len(server_chunk):
-				self.server_data = self.server_data[len(server_chunk):]
-			self.server_data_semaphore.release()
-			if len(server_chunk):
-				return server_chunk
-			# 3.) Check if we did anything at all and take a nap if not so:
-			if client_read_length==0 and not len(server_chunk):
-				time.sleep(0.1)
-		self.app.trace( "WebSocket-Shutdown (read %s)" % (self.app.query.remote_addr) )
-		# return zero size buffer to signalize eof:
-		return b''
+		# 1.) Check for decoded frames from the client and forward them
+		# to the server-side endpoint for consumtion:
+		self.client_data_semaphore.acquire()
+		client_message_count = 0
+		while self.client_messages:
+			self.onmessage( self.client_messages.pop() )
+			client_message_count += 1
+		self.client_data_semaphore.release()
+		# 2.) Check if we have frame data from send calls of the server-side endpoint,
+		# that we can forward to the WebSocket server:
+		self.server_data_semaphore.acquire()
+		server_chunk = self.server_data
+		if len(server_chunk):
+			self.server_data = self.server_data[len(server_chunk):]
+		self.server_data_semaphore.release()
+		return server_chunk
 	
 	def close( self ):
 		self.app.trace( "WebSocket-Shutdown (closing %s)..." % (self.app.query.remote_addr) )
 		# request Termination on the server-side application endpoint:
-		if endpoint:
-			self.endpoint.stop()
-			self.endpoint.join() # wait on the endpoint thread
+		self.endpoint.stop()
+		self.endpoint.join() # wait on the endpoint thread
 		# signalize Termination to input and output subroutines:
 		self.quitting = True
 		self.client_reader.join() # wait on client reader thread

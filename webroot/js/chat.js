bc0da1149b788c9abde66ab63ec346df44f6cabd
syslock <syslock@gmx.de>
2017-07-23 00:35:30 +0200

    implemented experimental user online list provided by ws chat server

--- webroot/js/chat.js
+++ webroot/js/chat.js
@@ -32,15 +32,27 @@ var Chat = function() {
 	my.connected = function( event ) {
 		my.connect_retry_count=0;
 		if( GlobalStatusBar ) {
-			GlobalStatusBar.add_message( {text:"Chat WebSocket connected", emblem_css:{"background-color":"green"}} );
+			GlobalStatusBar.add_message( {text:"Chat connected", css:{"color":"gray"}, emblem_css:{"background-color":"green"}} );
 		}
 	};
 	
 	my.recv = function( event ) {
 		var msg = parse_result( event.data );
 		if( GlobalStatusBar ) {
-			GlobalStatusBar.add_message( {source: msg.src, text:msg.msg, emblem_css:{"background-color":"blue"}} );
-			GlobalStatusBar.show();
+			if( msg.msg_type == "text" ) {
+				GlobalStatusBar.add_message( {source: msg.src, text:msg.msg, emblem_css:{"background-color":"blue"}} );
+				GlobalStatusBar.show();
+			} else if( msg.msg_type == "join" || msg.msg_type == "leave" ) {
+				online_string = "now online: "+JSON.stringify(msg.msg.nick_list)
+				if( msg.msg_type == "join" ) {
+					GlobalStatusBar.add_message( {text:"User '"+msg.msg.nick+"' connected, "+online_string, css:{"color":"gray"}, emblem_css:{"background-color":"green"}} );
+					if( update_user_list ) update_user_list( msg.msg.nick_list );
+				}
+				if( msg.msg_type == "leave" ) {
+					GlobalStatusBar.add_message( {text:"User '"+msg.msg.nick+"' disconnected, "+online_string, css:{"color":"gray"}, emblem_css:{"background-color":"orange"}} );
+					if( update_user_list ) update_user_list( msg.msg.nick_list );
+				}
+			}
 		}
 	};
 	
@@ -52,7 +64,7 @@ var Chat = function() {
 			// we use asymptotic growth for retry latencies, starting by 0 seconds, maximum 5 minutes:
 			var retry_latency = Math.round( my.asymptotic_growth_function( my.connect_retry_count, 5*60, 10000 ) );
 			if( GlobalStatusBar ) {
-				GlobalStatusBar.add_message( {text:"Chat WebSocket error: "+String(event)+" (retry in "+String(retry_latency)+"s)", class:"error"} );
+				GlobalStatusBar.add_message( {text:"Chat error: "+String(event)+" (retry in "+String(retry_latency)+"s)", class:"error", css:{"color":"gray"}} );
 			}
 			my.connect_retry_timer = window.setTimeout( my.connect, retry_latency*1000 );
 		}
